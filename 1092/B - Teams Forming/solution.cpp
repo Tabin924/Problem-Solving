87e2a75a0a270dc7ec3501245430cc5583c1fc6d
Tabin924 <tabin.naurose@gmail.com>
2026-09-17 14:54:34 +0600

    Add Teams Forming [B] from Codeforces

---
+++ 1092/B - Teams Forming/solution.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int sum = 0;
+    for(int i=0; i<n-1; i+=2){
+        sum = sum + a[i+1]-a[i];
+    }
+    cout << sum << endl;
+    return 0;
+}
