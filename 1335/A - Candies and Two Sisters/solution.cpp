c223fe1cb28b769c0854e3e95b19fd06e6e577f2
Tabin924 <tabin.naurose@gmail.com>
2026-09-17 14:55:42 +0600

    Add Candies and Two Sisters [A] from Codeforces

---
+++ 1335/A - Candies and Two Sisters/solution.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int count = 0;
+        if(n%2==0){
+            count = (n / 2) - 1;
+        }
+        else{
+            count = n / 2;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
